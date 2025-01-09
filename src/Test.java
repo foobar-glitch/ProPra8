486adc130c449d99f8955c73c931aae22d1fb9be
Hamed <63098791+foobar-glitch@users.noreply.github.com>
2025-01-09 05:03:50 +0100

    Update and rename Main.java to Test.java

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+
+    public static void main(String[] args) {
+        Coordinate c00 = new Coordinate(0, 0);
+        Coordinate c10 = new Coordinate(1, 0);
+        Coordinate c01 = new Coordinate(0, 1);
+        Coordinate c11 = new Coordinate(1, 1);
+
+        Coordinate c20 = new Coordinate(2, 0);
+        Coordinate c21 = new Coordinate(2, 1);
+
+        Cube cube0 = new Cube(c00, c10, c01, c11);
+        Cube cube1 = new Cube(c01, c11, c20, c21);
+
+        Cube cubeGes = cube0.addCubes(cube1);
+
+    }
+}
